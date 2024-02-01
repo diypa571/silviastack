6be511f014192bce84192b66e554d246e1794ceb
Diyar Parwana <diypa571@gmail.com>
2024-02-01 22:11:01 +0100

    Update addHostCustom.sh

--- addHostCustom.sh
+++ addHostCustom.sh
@@ -18,46 +18,7 @@ ERRORS_FOLDER="$WEB_ROOT/errors"
 
 sudo mkdir -p $ERRORS_FOLDER
 sudo chown -R $USER:$USER $WEB_ROOT
-
-# Skapa error filerna 
-sudo tee "$ERRORS_FOLDER/error.php" > /dev/null <<EOL
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Error</title>
-</head>
-<body>
-    <h1>Error</h1>
-    <p>An error occurred. Error code: <?php echo http_response_code(); ?></p>
-    <?php
-        $error_code = http_response_code();
-        $error_message = "An error occurred.";
-        switch ($error_code) {
-            case 400:
-                $error_message = "Bad Request";
-                break;
-            case 401:
-                $error_message = "Unauthorized";
-                break;
-            case 403:
-                $error_message = "Forbidden";
-                break;
-            case 404:
-                $error_message = "Not Found";
-                break;
-            case 500:
-            case 502:
-            case 503:
-            case 504:
-                $error_message = "Internal Server Error";
-                break;
-        }
-    ?>
-    <p><?php echo $error_message; ?></p>
-</body>
-</html>
-EOL
-
+ 
 VHOST_FILE="/etc/apache2/sites-available/$DOMAIN.conf"
 sudo touch $VHOST_FILE
 
@@ -70,19 +31,19 @@ cat <<EOL | sudo tee $VHOST_FILE > /dev/null
     ErrorLog \${APACHE_LOG_DIR}/error.log
     CustomLog \${APACHE_LOG_DIR}/access.log combined
 
-    # Dynamic error page
-    ErrorDocument 400 /errors/error.php
-    ErrorDocument 401 /errors/error.php
-    ErrorDocument 403 /errors/error.php
-    ErrorDocument 404 /errors/error.php
-    ErrorDocument 500 /errors/error.php
-    ErrorDocument 502 /errors/error.php
-    ErrorDocument 503 /errors/error.php
-    ErrorDocument 504 /errors/error.php
+    ErrorDocument 400 /errors/error_400.html
+    ErrorDocument 401 /errors/error_401.html
+    ErrorDocument 403 /errors/error_403.html
+    ErrorDocument 404 /errors/error_404.html
+    ErrorDocument 500 /errors/error_500.html
+
+    <Directory "$WEB_ROOT/html">
+        AllowOverride All
+    </Directory>
 </VirtualHost>
 EOL
 
-
+# Enable virtual host and restart Apache
 sudo a2ensite $DOMAIN.conf
 sudo systemctl restart apache2
 
