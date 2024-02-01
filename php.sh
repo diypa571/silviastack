72ffcbdc33d8414c891fa6111df3d28f0f396d13
Diyar Parwana <diypa571@gmail.com>
2024-02-01 15:23:35 +0100

    Update php.sh

--- php.sh
+++ php.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
+#Diypa571
+PHP_VERSION="8.2"
 
 
+sudo add-apt-repository ppa:ondrej/php
 sudo apt update
 
+sudo apt install php${PHP_VERSION} libapache2-mod-php${PHP_VERSION} php${PHP_VERSION}-mysql
+pool_config_file="/etc/php/${PHP_VERSION}/fpm/pool.d/www.conf"
 
-sudo apt install php libapache2-mod-php php-mysql
-
+sudo apt update
+sudo apt install php${PHP_VERSION} libapache2-mod-php${PHP_VERSION} php${PHP_VERSION}-mysql
 
 php -v
+echo "Pool configuration file path: $pool_config_file"
