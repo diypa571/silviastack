6da592bc17b9bff5a9230706b271893fbbf31bc9
Diyar Parwana <diypa571@gmail.com>
2024-02-01 15:04:17 +0100

    Update addHost.sh

--- addHost.sh
+++ addHost.sh
@@ -5,24 +5,19 @@ if [ "$EUID" -ne 0 ]; then
     exit 1
 fi
 
-
 if [ -z "$1" ]; then
     echo "Usage: $0 <domain>"
     exit 1
 fi
 
-
 DOMAIN=$1
-
 
 WEB_ROOT="/var/www/$DOMAIN"
 sudo mkdir $WEB_ROOT
 sudo chown -R $USER:$USER $WEB_ROOT
 
-
 VHOST_FILE="/etc/apache2/sites-available/$DOMAIN.conf"
 sudo touch $VHOST_FILE
-
 
 cat <<EOL | sudo tee $VHOST_FILE > /dev/null
 <VirtualHost *:80>
@@ -35,10 +30,10 @@ cat <<EOL | sudo tee $VHOST_FILE > /dev/null
 </VirtualHost>
 EOL
 
-# Enable the virtual host
 sudo a2ensite $DOMAIN.conf
 
-# Restart Apache to apply changes
 sudo systemctl restart apache2
 
-echo "Virtual host for $DOMAIN created successfully."
+echo "127.0.0.1 $DOMAIN" | sudo tee -a /etc/hosts
+
+echo "Virtual host for $DOMAIN created successfully, and added to /etc/hosts."
