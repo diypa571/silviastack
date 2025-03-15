6869f721b46c5ad5368b15bfaa0c660580dc7fba
Di Par <diypa571@gmail.com>
2025-03-15 09:08:06 +0100

    Update php.sh

--- php.sh
+++ php.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
-#Diypa571, v 8.2 will be installed
-PHP_VERSION="8.2"
+# Author: Diypa571  
+# Script to install PHP 8.3 and necessary extensions  
 
-sudo apt update
-sudo add-apt-repository ppa:ondrej/php
-sudo apt update
- 
-sudo apt install php${PHP_VERSION} libapache2-mod-php${PHP_VERSION} php${PHP_VERSION}-mysql
+# Update package lists  
+sudo apt update  
 
-php -v
- 
+# Add PHP repository (if not already added)  
+sudo add-apt-repository -y ppa:ondrej/php  
+
+# Update package lists again after adding repository  
+sudo apt update  
+
+# Install PHP 8.3 and required extensions  
+PHP_VERSION="8.3"
+sudo apt install -y php${PHP_VERSION} php${PHP_VERSION}-cli php${PHP_VERSION}-fpm \
+php${PHP_VERSION}-mysql php${PHP_VERSION}-curl php${PHP_VERSION}-xml php${PHP_VERSION}-mbstring \
+php${PHP_VERSION}-zip php${PHP_VERSION}-gd php${PHP_VERSION}-bcmath libapache2-mod-php${PHP_VERSION}  
+
+# Set PHP 8.3 as default  
+sudo update-alternatives --set php /usr/bin/php${PHP_VERSION}  
+
+# Restart Apache and PHP-FPM services  
+sudo systemctl restart apache2  
+sudo systemctl restart php${PHP_VERSION}-fpm  
+
+# Display installed PHP version  
+php -v  
