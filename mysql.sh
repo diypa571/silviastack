48b693d29c941d22f87e3c146cb5294e6b2f8e59
Di Par <diypa571@gmail.com>
2025-03-16 10:55:31 +0100

    Update mysql.sh

--- mysql.sh
+++ mysql.sh
@@ -3,11 +3,12 @@
 sudo apt install mysql-server
 
 
-sudo mysql_secure_installation
+#sudo mysql_secure_installation
 
-sudo mysql
-ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password';
+#sudo mysql
+
+#ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password';
 exit
 
-echo "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password';"
-sudo mysql_secure_installation
+#echo "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password';"
+#sudo mysql_secure_installation
